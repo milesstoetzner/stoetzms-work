eba180d032717d1887f2f09ba9c943240c05b413
Miles Stötzner <miles@stoetzner.de>
2025-01-18 20:58:14 +0100

    docs

--- src/index.js
+++ src/index.js
@@ -10,59 +10,59 @@ const program = new Command()
 program.name('work').description('A simple utility for tracking working hours.')
 
 program.command('status')
-    .description('get status')
-    .option('--file [string]', '', config.file)
+    .description('get the current working status')
+    .option('--file [string]', `the working hours file`, config.file)
     .action(hae(async options => {
         await actions.status(options)
     }))
 
 program.command('start')
     .description('start tracking')
-    .option('--file [string]', '', config.file)
+    .option('--file [string]', `the working hours file`, config.file)
     .action(hae(async options => {
         await actions.start(options)
     }))
 
 program.command('stop')
     .description('stop tracking')
-    .option('--file [string]', '', config.file)
+    .option('--file [string]', `the working hours file`, config.file)
     .action(hae(async options => {
         await actions.stop(options)
     }))
 
 program.command('until')
     .description('until goal reached')
-    .option('--file [string]', '', config.file)
-    .option('--goal [string]', '')
+    .option('--file [string]', `the working hours file`, config.file)
+    .option('--goal [string]', 'the duration to work', '6h')
     .action(hae(async options => {
         await actions.until(options)
     }))
 
 program.command('edit')
-    .description('open file in editor')
-    .option('--file [string]', '', config.file)
+    .description('open the working hours file in editor')
+    .option('--file [string]', `the working hours file`, config.file)
     .action(hae(async options => {
         await actions.edit(options)
     }))
 
 program.command('cat')
-    .description('cat file')
-    .option('--file [string]', '', config.file)
+    .description('cat the working hours file')
+    .option('--file [string]', `the working hours file`, config.file)
     .action(hae(async options => {
         await actions.cat(options)
     }))
 
 program.command('drop')
-    .description('drop file')
-    .option('--file [string]', '', config.file)
+    .description('drop the working hours file')
+    .option('--file [string]', `the working hours file`, config.file)
     .option('--dangerous', '', false)
     .action(hae(async options => {
         await actions.drop(options)
     }))
 
 program.command('date')
-    .description('get date')
-    .option('--file [string]', '', config.file)
+    .description('get current date')
+    .option('--file [string]', `the working hours file`, config.file)
     .action(hae(async options => {
         await actions.date(options)
     }))
