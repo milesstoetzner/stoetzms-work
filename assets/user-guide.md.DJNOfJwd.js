6f2d6de86e6a37f2067a96c20ccca2ceb2a0d872
milesstoetzner <milesstoetzner@users.noreply.github.com>
2025-06-08 21:53:59 +0000

    deploy: c5b8065d4b3ff503fca87325d362ebd71f589d40

---
+++ assets/user-guide.md.DJNOfJwd.js
@@ -0,0 +1,17 @@
+import{_ as s,c as n,o as e,ae as t}from"./chunks/framework.BtEkjhVd.js";const u=JSON.parse('{"title":"User Guide","description":"","frontmatter":{},"headers":[],"relativePath":"user-guide.md","filePath":"user-guide.md"}'),i={name:"user-guide.md"};function p(l,a,o,d,r,c){return e(),n("div",null,a[0]||(a[0]=[t(`<h1 id="user-guide" tabindex="-1">User Guide <a class="header-anchor" href="#user-guide" aria-label="Permalink to &quot;User Guide&quot;">​</a></h1><h2 id="installation" tabindex="-1">Installation <a class="header-anchor" href="#installation" aria-label="Permalink to &quot;Installation&quot;">​</a></h2><div class="language-shell vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">shell</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">npm</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> install</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -g</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> stoetzms-work</span></span></code></pre></div><h2 id="usage" tabindex="-1">Usage <a class="header-anchor" href="#usage" aria-label="Permalink to &quot;Usage&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Usage: work [options] [command]</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>A simple utility for tracking working hours.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Options:</span></span>
+<span class="line"><span>  -h, --help        display help for command</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Commands:</span></span>
+<span class="line"><span>  status [options]  get status</span></span>
+<span class="line"><span>  start [options]   start tracking</span></span>
+<span class="line"><span>  stop [options]    stop tracking</span></span>
+<span class="line"><span>  until [options]   until goal reached</span></span>
+<span class="line"><span>  edit [options]    open file in editor</span></span>
+<span class="line"><span>  cat [options]     cat file</span></span>
+<span class="line"><span>  drop [options]    drop file</span></span>
+<span class="line"><span>  date [options]    get date</span></span>
+<span class="line"><span>  help [command]    display help for command</span></span></code></pre></div><h2 id="global-options" tabindex="-1">Global Options <a class="header-anchor" href="#global-options" aria-label="Permalink to &quot;Global Options&quot;">​</a></h2><table tabindex="0"><thead><tr><th>Option</th><th>Description</th><th>Default</th></tr></thead><tbody><tr><td><code>--config</code></td><td></td><td><code>~/stoetzms-work.log</code></td></tr></tbody></table>`,7)]))}const g=s(i,[["render",p]]);export{u as __pageData,g as default};
