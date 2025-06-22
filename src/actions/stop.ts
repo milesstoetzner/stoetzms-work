4677dfdfc3272598851e2a45b2a721884d8be4b4
Miles Stötzner <miles@stoetzner.de>
2025-06-22 22:17:49 +0200

    cleanup strict

--- src/actions/stop.ts
+++ src/actions/stop.ts
@@ -2,18 +2,13 @@ import * as repository from '../repository'
 import * as utils from '../utils'
 import actions from './index'
 
-export type StopOptions = {file?: string; strict?: boolean}
+export type StopOptions = {file?: string}
 
 export default async function (options: StopOptions) {
-    options.strict = options.strict ?? false
-
     const entries = await repository.load(options.file)
 
     const latest = utils.last(entries)
-    if (latest && latest.type === repository.STOP) {
-        if (options.strict) throw new Error('Already stopped ...')
-        return await actions.status(options)
-    }
+    if (latest && latest.type === repository.STOP) return await actions.status(options)
 
     await actions.status(options)
     await repository.add(repository.STOP, options.file)
